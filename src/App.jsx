debf4aceb15cb784cc53c86750a0fae674d14bbd
Christian Vilca <christian.vilcaq@gmail.com>
2020-04-17 17:32:27 -0500

    Inicializar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hola</h1>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom' // Permite agregar al DOM la aplicacion hecha en react
+import { HelloWorld } from './App.jsx'
+
+ReactDOM.render(<HelloWorld />,document.getElementById('app'));
