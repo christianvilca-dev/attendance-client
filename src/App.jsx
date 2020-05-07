224b0ad0b2ac1f6d556e4cbf3a451a1147fba262
Christian Vilca <christian.vilcaq@gmail.com>
2020-05-07 17:31:54 -0500

    Update Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React, { Component, lazy, Suspense } from 'react'
 import Spinner from './components/Atoms/Spinner.jsx'
+import {Provider} from 'react-redux'
+import store from './redux/store'
+import {getAllConfirmantes} from './redux/actionCreators.js'
+import AppRoutes from './AppRoutes.jsx'
 /* import HeaderMain from './components/Organisms/HeaderMain.jsx'
 import Main from './components/Organisms/Main.jsx'
 import Header from './components/Organisms/Header.jsx' */
@@ -8,16 +12,20 @@ const HeaderMain = lazy(() => import('./components/Organisms/HeaderMain.jsx'))
 const Main = lazy(() => import('./components/Organisms/Main.jsx'))
 //const Header = lazy(() => import('/components/Organisms/Header.jsx'))
 
-export class App extends Component {
-  render() {
+/* store.dispatch(getAllConfirmantes()) */
+
+const App = () => {
     return (
       <>
-        <Suspense fallback={<Spinner />}>
-          <HeaderMain />
-          {/* <Header /> */}
-          <Main />
-        </Suspense>
+       {/*  <Provider store={store}> */}
+          {/* <Suspense fallback={<Spinner />}>
+            <HeaderMain />
+            <Main />
+          </Suspense> */}
+          <AppRoutes />
+        {/* </Provider> */}
       </>
     )
-  }
 }
+
+export default App
